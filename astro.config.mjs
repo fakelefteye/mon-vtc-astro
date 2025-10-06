bf4c85f4bb4105bee0c727035ca89d7f5316179f
vtc <fakelefteye@gmail.com>
2025-10-06 18:05:29 +0200

    astro.co,fig.msj , layout with cookieconsent

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,8 @@ import { defineConfig } from 'astro/config';
 import vercel from '@astrojs/vercel';
 
 import sitemap from '@astrojs/sitemap';
+import vercelAnalytics from '@vercel/analytics/astro';
+import vercelSpeedInsights from '@vercel/speed-insights/astro';
 
 export default defineConfig({
   site: 'https://transfert-aeroport-grenoble.fr',
@@ -17,5 +19,9 @@ export default defineConfig({
   },
 
   adapter: vercel(),
-  integrations: [sitemap()],
+  integrations:  [
+    sitemap(),
+    vercelAnalytics(), // Active l'intégration Analytics
+    vercelSpeedInsights(), // Active l'intégration Speed Insights
+  ],
 });
