a0dbcb4dd0ade19b86ee01ae3b13ebb6550376e7
vtc <fakelefteye@gmail.com>
2025-10-15 14:00:31 +0200

    add robot.txt et sitemap

--- src/components/astro.config.mjs
+++ src/components/astro.config.mjs
@@ -3,11 +3,11 @@ import react from '@astrojs/react';
 // import sitemap from '@astrojs/sitemap'; // Commentez cette ligne
 import vercel from '@astrojs/vercel'; // Si vous l'avez
 
+import sitemap from '@astrojs/sitemap';
+
 export default defineConfig({
   site: 'https://transfert-aeroport-grenoble.fr', // Conservez votre site URL
   adapter: vercel(), // Conservez votre adaptateur
-  integrations: [
-    react(),
-    // sitemap(), // Commentez cette ligne
-  ],
+  integrations: [// sitemap(), // Commentez cette ligne
+  react(), sitemap()],
 });
